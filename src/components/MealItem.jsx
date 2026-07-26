92c589b7c0a69dba9a51e8eff9e6e54755f66803
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-26 14:07:53 -0400

    Fix the missing images with the correct backend URL

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -2,7 +2,7 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={meal.image} alt={meal.name} />
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">{meal.price}</p>
