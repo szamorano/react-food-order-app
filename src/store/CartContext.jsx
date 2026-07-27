f1958d62d8de548f6a22d3474039ba3d9b88136f
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-27 11:06:38 -0400

    Added useReducer hook to manage state.

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
   items: [],
@@ -6,7 +6,21 @@ const CartContext = createContext({
   removeItem: (id) => {},
 });
 
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    //.....
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //.....
+  }
+
+  return state;
+}
+
 export function CartContextProvider({ children }) {
+  useReducer();
+
   return <CartContext.Provider>{children}</CartContext.Provider>;
 }
 
