85bd9abc579609ff065abf12caaf676b8013510f
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-27 17:53:30 -0400

    Open the dialog element programmatically.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-export default function Modal({ children, open }) {
+export default function Modal({ children, open, className = "" }) {
   const dialog = useRef();
 
   useEffect(() => {
     if (open) {
+      dialog.current.showModal();
     }
   }, [open]);
 
   return createPortal(
-    <dialog ref={dialog}>{children}</dialog>,
+    <dialog ref={dialog} className={`modal ${className}`}>
+      {children}
+    </dialog>,
     document.getElementById("modal"),
   );
 }
