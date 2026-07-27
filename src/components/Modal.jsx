99386f6ce2d745854a5b6cd01cdc00f7e1c288a5
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-27 17:39:44 -0400

    Created reusable cart modal component, added useRef

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 export default function Modal({ children, open }) {
+  const dialog = useRef();
+
   useEffect(() => {
     if (open) {
     }
   }, [open]);
 
   return createPortal(
-    <dialog>{children}</dialog>,
+    <dialog ref={dialog}>{children}</dialog>,
     document.getElementById("modal"),
   );
 }
