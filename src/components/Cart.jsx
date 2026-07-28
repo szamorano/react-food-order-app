c707429f13be18fdd7198aa6f7066cd57c3edc31
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-28 14:19:23 -0400

    Handle close cart functionality with added cleanup function in useEffect

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -14,6 +14,10 @@ export default function Cart() {
     0,
   );
 
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
   return (
     <Modal className="cart" open={userProgressCtx.progress === "cart"}>
       <h2>Your Cart</h2>
@@ -26,8 +30,10 @@ export default function Cart() {
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
-        <Button>Go to Checkout</Button>
+        <Button textOnly onClick={handleCloseCart}>
+          Close
+        </Button>
+        <Button onClick={handleCloseCart}>Go to Checkout</Button>
       </p>
     </Modal>
   );
