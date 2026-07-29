1f97026e4f6a65c6353ee5d4ccc72b26165246e4
Steven Zamorano <stevenzamorano.code@gmail.com>
2026-07-29 13:25:36 -0400

    Improve user experience sending order request, with a useHttp custom hook.

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,19 @@
+// for sending general requests
+async function sendHttpRequest(url, config) {
+  const response = await fetch(url, config);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(
+      resData.message || "Something went wrong, failed to send request.",
+    );
+  }
+
+  return resData;
+}
+
+export default function useHttp() {
+  // for updating some state based on the request status
+  function sendRequest() {}
+}
